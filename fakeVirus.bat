28f8ce9f7cdd72dd0d57f2f6052cfd7828f16322
TownerK91 <kevintowner@gmail.com>
2017-11-30 10:29:12 -0500

    Add files via upload

---
+++ fakeVirus.bat
@@ -0,0 +1,18 @@
+@echo off
+color 04
+echo FATAL Error:::Your computer is under attack
+echo PRESS TAB TO HINDER FATAL
+PAUSE>NULL
+:top
+
+echo !DELETING USERS
+echo !DELETING \C:\INTEL
+echo !DELETING .html
+echo !DELETING .txt
+echo !DELETING .msg
+echo !DELETING outlook files
+echo !DELETING .doc
+echo !DELETING .docx
+echo !DELETING .js
+echo !DELETING .hahaGOtYOu
+goto top
